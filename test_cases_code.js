b5b657a0d39700f9fe66f276a9a329aa9c4fe8a2
Abhik Patra <theabhik2020@gmail.com>
2024-10-13 00:01:15 +0530

    test_case in postman

---
+++ test_cases_code.js
@@ -0,0 +1,17 @@
+pm.test("Status code is 200", function () {
+    pm.response.to.have.status(200);
+});
+
+
+const response = pm.response.json();
+
+console.log(response.status);
+//if there exits any to-me/ - like this you can use
+console.log(response['status'])
+
+
+pm.test('It is ok',()=>{
+    pm.expect(response.status).to.eql('OK');
+})
+
+
